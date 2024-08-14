f7e2880f8b8d6bb0807e7e9cf130f8629dc10c2e
Albert Anyika <disiluk15@gmail.com>
2024-08-13 18:42:20 -0700

    if_statements

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /etc/
 
 file {'/etc/school':
-  path => '/etc/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  path    => '/etc/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
